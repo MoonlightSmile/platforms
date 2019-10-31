45f1488c97d7c8f2d80a237446c0a2642542f5c3
moonlight <362652565@qq.com>
2019-10-31 16:14:57 +0800

    增加人物动画

--- src/sprite/Player.ts
+++ src/sprite/Player.ts
@@ -12,18 +12,72 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.world.enableBody(this);
     // this.setBounceY(0.2);
-
+    this._initAnims();
+    this.play("stop");
     this.setCollideWorldBounds(true);
   }
-  public move(step: number) {
-    this.body.setVelocityX(step * this.speed);
+  private _initAnims(): void {
+    this.scene.anims.create({
+      key: "stop",
+      frames: [{ key: "hero", frame: 0 }]
+    });
+    this.scene.anims.create({
+      key: "run",
+      frames: [1, 2].map(frame => ({
+        key: "hero",
+        frame
+      })),
+      frameRate: 8,
+      repeat: -1
+    });
+    this.scene.anims.create({
+      key: "jump",
+      frames: [{ key: "hero", frame: 3 }]
+    });
+    this.scene.anims.create({
+      key: "fall",
+      frames: [{ key: "hero", frame: 4 }]
+    });
   }
-  public jump() {
+  private _getAnimationName(): string {
+    let name = "stop";
+    const {
+      velocity: { x, y },
+      touching: { down }
+    } = this.body;
+    if (y < 0) {
+      name = "jump";
+    } else if (y >= 0 && !down) {
+      name = "fall";
+    } else if (x !== 0 && down) {
+      name = "run";
+    }
+    return name;
+  }
+  public bound(): void {
+    this.setVelocityY(-200);
+  }
+  public move(step: number): void {
+    this.body.setVelocityX(step * this.speed);
+
+    if (step > 0) {
+      this.setFlipX(false);
+    } else if (step < 0) {
+      this.setFlipX(true);
+    }
+  }
+  public die(): void {
+    this.scene.scene.restart();
+  }
+  public jump(): void {
     const canJump = this.body.touching.down;
     if (canJump) {
       this.body.setVelocityY(-600);
       this.scene.sound.play("sfx:jump");
     }
+  }
+  public update(): void {
+    this.play(this._getAnimationName(), true);
   }
 }
 export default Player;
