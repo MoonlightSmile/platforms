40f8f1b05594b9dbc2137d99fc9e930a3f4872d5
moonlight <362652565@qq.com>
2019-10-31 16:15:57 +0800

    添加动画

--- src/scene/Start.ts
+++ src/scene/Start.ts
@@ -8,29 +8,59 @@ class Start extends Phaser.Scene {
   public hero: Player;
   public platforms: Phaser.Physics.Arcade.StaticGroup;
   public coins: Phaser.Physics.Arcade.StaticGroup;
-  public spiders: Phaser.Physics.Arcade.Group;
+  public spiders: Phaser.GameObjects.Group;
   public enemyWalls: Phaser.Physics.Arcade.StaticGroup;
+  public hud: Phaser.GameObjects.Group;
+  public coinPickupCount: number = 0;
+  public count: Phaser.GameObjects.BitmapText;
   constructor() {
     super({ key: "start" });
   }
 
   public create() {
     this.background = this.add.image(0, 0, "background").setOrigin(0, 0);
+    // this.sound.play("bgm", { loop: true });
     this.platforms = this.physics.add.staticGroup();
     this.coins = this.physics.add.staticGroup();
     this.enemyWalls = this.physics.add.staticGroup();
-    this.spiders = this.physics.add.group();
+    this.spiders = this.add.group();
+    this.hud = this.add.group();
     this._loadLevel(this.cache.json.get("level:1"));
     this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this._createHud();
+
     this.physics.add.collider(this.hero, this.platforms);
     this.physics.add.collider(this.spiders, this.platforms);
     this.physics.add.collider(this.spiders, this.platforms);
     this.physics.add.collider(this.spiders, this.enemyWalls);
+
     this.physics.add.overlap(this.hero, this.coins, this._eatCoin, null, this);
+    this.physics.add.overlap(
+      this.hero,
+      this.spiders,
+      this._onHeroVsEnemy,
+      null,
+      this
+    );
   }
   private _eatCoin(hero: Player, coin: Phaser.GameObjects.GameObject): void {
+    this.coinPickupCount += 1;
     this.sound.play("sfx:coin");
     coin.destroy();
+  }
+  private _createHud(): void {
+    // ...
+    const icon = this.hud
+      .create(0, 0, "icon:coin")
+      .setOrigin(0, 0)
+      .setPosition(10, 10);
+    this.count = this.add.bitmapText(
+      icon.x + icon.width + 5,
+      icon.height / 2,
+      "font",
+      "0"
+    );
+    this.hud.add(this.count);
   }
   private _loadLevel(json: Level): void {
     const { platforms, hero, coins, spiders } = json;
@@ -43,11 +73,21 @@ class Start extends Phaser.Scene {
     const { x, y } = spider;
     this.spiders.add(new Spider(this, x, y));
   }
+  private _onHeroVsEnemy(hero: Player, spider: Spider): void {
+    if (hero.body.velocity.y) {
+      hero.bound();
+      spider.die();
+      this.sound.play("sfx:stomp");
+    } else {
+      this.sound.play("sfx:stomp");
+      hero.die();
+    }
+  }
   private _spawnEnemyWall(x: number, y: number, side: "left" | "right") {
     this.enemyWalls
       .create(x, y, "invisible-wall")
       .setOrigin(side === "left" ? 1 : 0, 1)
-      .refreshBody();
+      .refreshBody().visible = false;
   }
   private __spawnPlatform(platform: Point): void {
     const { x, y, image } = platform;
@@ -79,7 +119,9 @@ class Start extends Phaser.Scene {
 
   public update() {
     this._inputHander();
+    this.count.text = `x${String(this.coinPickupCount)}`;
     this.spiders.getChildren().forEach(spider => spider.update());
+    this.hero.update()
   }
 }
 
